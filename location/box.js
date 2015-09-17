3dd77e277442890d0080ec363c1583539271448f
Jesus Prieto Colomina <chus1818@gmail.com>
2015-09-17 22:19:09 +0100

    Refactoring boxer

--- location/box.js
+++ location/box.js
@@ -22,43 +22,9 @@ var box = (function() {
     return function(){ return get(name) }
   }
 
-  let('router', function(){
-    var express = require('express');
-    return express.Router();
-  });
-
-  let('mmdbPath', function(){ return './GeoLite2-Country.mmdb' });
-
-  let('mmdbConnection', function(){
-    var MMDBReader = require('mmdb-reader');
-    return new MMDBReader(get('mmdbPath'));
-  });
-
-  let('mmdbAdapter', function(){
-    var mmdbAdapter = require('./lib/adapters/mmdb');
-    return mmdbAdapter({
-      connection: get('mmdbConnection')
-    });
-  });
-
-  let('countriesCollection', function(){
-    var countriesCollection = require('./lib/countries/collection');
-    return countriesCollection({
-      adapter: get('mmdbAdapter')
-    });
-  })
-
-  let('countriesApi', function(){
-    var countriesApi = require('./lib/countries/api');
-    return countriesApi({
-      collection: get('countriesCollection'),
-      router: get('router')
-    });
-  });
-
-  return {
+  var boxer = {
     router: serve('router'),
-    persistence: {
+    repos: {
       mmdb: {
         connection: serve('mmdbConnection'),
         path: serve('mmdbPath')
@@ -75,6 +41,42 @@ var box = (function() {
       api: serve('countriesApi')
     }
   };
+
+  let('router', function(){
+    var express = require('express');
+    return express.Router();
+  });
+
+  let('mmdbPath', function(){ return './GeoLite2-Country.mmdb' });
+
+  let('mmdbConnection', function(){
+    var MMDBReader = require('mmdb-reader');
+    return new MMDBReader(boxer.repos.mmdb.path());
+  });
+
+  let('mmdbAdapter', function(){
+    var mmdbAdapter = require('./lib/adapters/mmdb');
+    return mmdbAdapter({
+      connection: boxer.repos.mmdb.connection()
+    });
+  });
+
+  let('countriesCollection', function(){
+    var countriesCollection = require('./lib/countries/collection');
+    return countriesCollection({
+      adapter: boxer.adapters.mmdb()
+    });
+  })
+
+  let('countriesApi', function(){
+    var countriesApi = require('./lib/countries/api');
+    return countriesApi({
+      collection: boxer.countries.collection(),
+      router: boxer.router()
+    });
+  });
+
+  return boxer;
 })();
 
 module.exports = box;
